2e98f92907bda6bea7fcd9004454b6d1346e010d
Juxhin Xhihani <Juxhin.Xhihani@raiffeisen.al>
2023-06-21 17:15:47 +0200

    Add baseic files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foodmine';
+  isShow=true;
+  showHideNav(event: any){
+    this.isShow= !(event instanceof LoginComponent);
+
+  }
 }
