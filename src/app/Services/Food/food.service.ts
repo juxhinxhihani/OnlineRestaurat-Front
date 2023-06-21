3c85c3613c5c04a1e15a9624f8c2e4a39e0f9f4e
Juxhin Xhihani <Juxhin.Xhihani@raiffeisen.al>
2023-06-21 17:13:53 +0200

    add

---
+++ src/app/Services/Food/food.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  getAll(): String[]{
+    return  [
+      '/assets/Images/food-1.jpg',
+      '/assets/Images/food-2.jpg',
+      '/assets/Images/food-3.jpg',
+      '/assets/Images/food-4.jpg',
+      '/assets/Images/food-5.jpg',
+      '/assets/Images/food-6.jpg'
+      ]
+  }
+
+}
+
